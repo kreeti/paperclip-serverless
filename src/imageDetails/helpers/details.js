0c980fcb3a93bdf5e2d6399874acfa51247b6a45
Shalini <shalini.bhawsingka@kreeti.com>
2020-03-19 17:56:37 +0530

    cleanup code

---
+++ src/imageDetails/helpers/details.js
@@ -0,0 +1,24 @@
+//@ts-check
+const S3Service = require('../../../services/s3Service');
+const Image = require('../../../utils/Image');
+/**
+ *
+ * @param {string} imagekey
+ * @param {S3Service} s3service
+ */
+module.exports = async function validateImage(imagekey, s3service) {
+  const { cleanup, filePath, fileName } = await s3service.download(imagekey);
+
+  const image = new Image(filePath);
+  const metadata = await image.getMetaData();
+  const imageFormat = metadata.format;
+  const size = await image.getFileSize();
+  cleanup();
+  return {
+    fileName,
+    imageFormat,
+    size,
+    height: metadata.height,
+    width: metadata.width,
+  };
+}
